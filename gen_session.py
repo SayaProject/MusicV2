c81c1c4f96504ac383924f08cd6054bfa1ec8dd5
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-07-01 11:04:01 +0530

    Update gen_session.py

--- gen_session.py
+++ gen_session.py
@@ -1,8 +1,8 @@
 import asyncio
 from pyrogram import Client
 
-API_ID = 30201492
-API_HASH = "6bf0844e2bd6dc434fa19c641deeaf84"
+API_ID = 30422005
+API_HASH = "5170ded206641d73215baf40175a6924"
 
 async def generate():
     async with Client("session_gen", api_id=API_ID, api_hash=API_HASH) as app:
